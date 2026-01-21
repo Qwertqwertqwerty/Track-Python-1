83fcbe5f514f89150236c199fcbb54285b6b3dfb
Qwertqwertqwerty <dima-karhu@mail.ru>
2026-01-21 20:13:52 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/3_задание.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+half_index = len(list_players) // 2
+
+pervaya_team = list_players[:half_index]
+vtoraya_team = list_players[half_index:]
+
+print(pervaya_team)
+print(vtoraya_team)
